fb62e9ca4f3af798cf338b93648a0600ab37bcde
Farkhadzz <zulfugarovfarhad6@gmail.com>
2024-04-26 02:45:58 +0400

    updated card and added fixed card size

--- ebook/src/components/card/Card.jsx
+++ ebook/src/components/card/Card.jsx
@@ -1,39 +1,65 @@
-import React from "react"
+import React from "react";
 
-function Card({ product }) {
-    const { description } = product;
-    const words = description.split(' ');
-    const limitedText = words.slice(0, 15).join(' ');
-
+function Card({ imageSrc, category, title, description, price }) {
     return (
-        <>
-            <div className="product-card">
-                <div className="badge">Hot</div>
-                <div className="product-tumb">
-                    <img src="https://i.imgur.com/xdbHo4E.png" />
-                </div>
-                <div className="product-details">
-                    <span className="product-catagory">Women,bag</span>
-                    <h4><a href="">Women leather bag</a></h4>
-                    {/* // когда надо будет отображать описание из DB используйте limitedText : */}
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!</p> {/*limitedText*/}
-                    <div className="product-bottom-details">
-                        <div className="product-price"><small>$96.00</small>$230.99</div>
-                        <div className="product-links">
-                            <a href=""><i className="fa fa-heart"></i></a>
-                            <a href=""><i className="fa fa-shopping-cart"></i></a>
-                        </div>
+        <div className="product-card">
+            <div className="product-tumb">
+                <img src={imageSrc} alt={title} />
+            </div>
+            <div className="product-details">
+                <span className="product-catagory">{category}</span>
+                <h4><a href="">{title}</a></h4>
+                <p>{description}</p>
+                <div className="product-bottom-details">
+                    <div className="product-price">${price}</div>
+                    <div className="product-links">
+                        <a href=""><i className="fa fa-heart"></i></a>
+                        <a href=""><i className="fa fa-shopping-cart"></i></a>
                     </div>
                 </div>
             </div>
-        </>
-    )
+        </div>
+    );
 }
 
 export default Card;
 
-// В функции Card мы ожидаем получить объект product через props. Этот объект содержит информацию о продукте, включая описание.
-// Мы достаем описание продукта из объекта product и сохраняем его в переменной description.
-// Затем мы разбиваем описание на отдельные слова, используя метод split(' '), который разделяет строку на массив строк по пробелам.
-// После этого мы выбираем только первые 15 слов из массива words с помощью метода slice(0, 15) и объединяем их обратно в строку с помощью метода join(' '). Это и есть наш ограниченный текст, который мы хотим показать.
-// Возвращаем JSX-элемент, который содержит структуру карточки продукта, но вместо полного описания мы выводим limitedText, который содержит только первые 15 слов.
+
+// import React from "react";
+
+// function Card({ product }) {
+//     const { description } = product;
+//     const words = description.split(' ');
+//     let limitedText = words.slice(0, 15).join(' ');
+
+//     if (words.length > 15) {
+//         limitedText += '...';
+//     }
+
+//     return (
+//         <>
+//             <div className="product-card">
+//                 <div className="badge">Hot</div>
+//                 <div className="product-tumb">
+//                     <img src="https://i.imgur.com/xdbHo4E.png" />
+//                 </div>
+//                 <div className="product-details">
+//                     <span className="product-catagory">Women,bag</span>
+//                     <h4><a href="">Women leather bag</a></h4>
+//                     <p>{limitedText}</p>
+//                     <div className="product-bottom-details">
+//                         <div className="product-price"><small>$96.00</small>$230.99</div>
+//                         <div className="product-links">
+//                             <a href=""><i className="fa fa-heart"></i></a>
+//                             <a href=""><i className="fa fa-shopping-cart"></i></a>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </>
+//     )
+// }
+
+// export default Card;
+
+// Этот код надо будет использовать уже когда мы возьмем описание с дб. Тут стоит ограничение description на 15 слов 
--- ebook/src/components/collections/Collection.jsx
+++ ebook/src/components/collections/Collection.jsx
@@ -1,33 +1,20 @@
 import React from "react";
+import Card from "../card/Card";
 
 function Collection() {
     return (
-        <>
-            <section className="section featured animate__animated animate__zoomIn" aria-label="featured collection">
-                <div className="container">
-                    <div className="product-card">
-                        <div className="product-tumb">
-                            <img src="book-4.png" />
-                        </div>
-                        <div className="product-details">
-                            <span className="product-catagory">Action</span>
-                            <h4><a href="">Some Book</a></h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!</p>
-                            <div className="product-bottom-details">
-                                <div className="product-price">
-                                    $12.99
-                                </div>
-                                <div className="product-links">
-                                    <a href=""><i className="fa fa-heart"></i></a>
-                                    <a href=""><i className="fa fa-shopping-cart"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
+        <section className="section featured animate__animated animate__zoomIn" aria-label="featured collection">
+            <div className="container">
+                <Card
+                    imageSrc="book-4.png"
+                    category="Action"
+                    title="Some Book"
+                    description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!"
+                    price="12.99"
+                />
+            </div>
+        </section>
+    );
 }
 
 export default Collection;
